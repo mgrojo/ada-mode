d4d331a13bb61afdf7e0bdf307653c1a0d2d61e4
stephen_leake-2 <stephen_leake-2@stephe-leake.org>
2011-12-03 23:12:10 +0000

    * progs/arm_frmd.adb: New file. * progs/arm_frmd.ads: New file.

---
+++ progs/arm_frmd.adb
@@ -0,0 +1,72 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<HTML>
+<TITLE>CVS log for arm/progs/arm_frmd.adb</TITLE>
+<h1><font color="#008000">CVS log for arm/progs/arm_frmd.adb</font></h1>
+<div style="font-size: 120%; margin-top: 1.0em; margin-bottom: 0.5em">
+<img src="/icons/back.gif" style="float: left">
+<a href="/cgi-bin/cvsweb.cgi/arm/progs/">to arm/progs/</a></div>
+<div style="margin-top: 0.5em; margin-bottom: 0.7em">
+<a href="#diff">Request differences between arbitrary revisions</a></div>
+<hr noshade>
+<span style="font-size: 120%"><b><a href="/cgi-bin/cvsweb.cgi/arm/progs/arm_frmd.adb?rev=1.5">1.5</a></b></span><br>
+Modified <i>2011/10/27 01:29:48</i>
+by <i>randy</i> with line changes <i>+7 -0</i><br>
+CVS Tags: <b>HEAD, Amend2_D14</b><br>
+<a href="/cgi-bin/cvsweb.cgi/arm/progs/arm_frmd.adb?diffbase=1.4&rev=1.5">Differences from 1.4</a><br>
+<div style="font-family: sans-serif; margin-top: 1.0em; margin-bottom: 0.5em">
+Various added and revised commands to fix problems in Ada 2012 Standard.
+</div><hr noshade>
+<span style="font-size: 120%"><b><a href="/cgi-bin/cvsweb.cgi/arm/progs/arm_frmd.adb?rev=1.4">1.4</a></b></span><br>
+Modified <i>2011/10/21 06:40:45</i>
+by <i>randy</i> with line changes <i>+6 -0</i><br>
+<a href="/cgi-bin/cvsweb.cgi/arm/progs/arm_frmd.adb?diffbase=1.3&rev=1.4">Differences from 1.3</a><br>
+<div style="font-family: sans-serif; margin-top: 1.0em; margin-bottom: 0.5em">
+Implemented various commands and parameters to fix problems in Ada 2012 Standard.
+</div><hr noshade>
+<span style="font-size: 120%"><b><a href="/cgi-bin/cvsweb.cgi/arm/progs/arm_frmd.adb?rev=1.3">1.3</a></b></span><br>
+Modified <i>2011/10/19 23:58:04</i>
+by <i>randy</i> with line changes <i>+1 -1</i><br>
+<a href="/cgi-bin/cvsweb.cgi/arm/progs/arm_frmd.adb?diffbase=1.2&rev=1.3">Differences from 1.2</a><br>
+<div style="font-family: sans-serif; margin-top: 1.0em; margin-bottom: 0.5em">
+Fixed minor glitch in copyright message.
+</div><hr noshade>
+<span style="font-size: 120%"><b><a href="/cgi-bin/cvsweb.cgi/arm/progs/arm_frmd.adb?rev=1.2">1.2</a></b></span><br>
+Modified <i>2011/10/18 23:27:29</i>
+by <i>randy</i> with line changes <i>+14 -16</i><br>
+<a href="/cgi-bin/cvsweb.cgi/arm/progs/arm_frmd.adb?diffbase=1.1&rev=1.2">Differences from 1.1</a><br>
+<div style="font-family: sans-serif; margin-top: 1.0em; margin-bottom: 0.5em">
+Changed to GPLv3 license at request of WG 9.
+</div><hr noshade>
+<span style="font-size: 120%"><b><a href="/cgi-bin/cvsweb.cgi/arm/progs/arm_frmd.adb?rev=1.1">1.1</a></b></span><br>
+Modified <i>2011/08/09 04:17:29</i>
+by <i>randy</i><br>
+<div style="font-family: sans-serif; margin-top: 1.0em; margin-bottom: 0.5em">
+Split additional stuff out of ARM_Format; added Aspect Description commands.
+</div>
+<hr noshade>
+<p><a name="diff">This form</a> allows you to request differences between any
+two revisions of a file, by number or by tag.</p>
+<form method="GET" action="/cgi-bin/cvsweb.cgi/arm/progs/arm_frmd.adb">
+Differences between
+<select name="diffbase">
+<option selected value="1.5">1.5
+<option value="1.4">1.4
+<option value="1.3">1.3
+<option value="1.2">1.2
+<option value="1.1">1.1
+<option value="1.5">HEAD
+<option value="1.5">Amend2_D14
+</select>
+<select name="rev">
+<option selected value="1.5">1.5
+<option value="1.4">1.4
+<option value="1.3">1.3
+<option value="1.2">1.2
+<option value="1.1">1.1
+<option value="1.5">HEAD
+<option value="1.5">Amend2_D14
+</select>
+<div style="margin-top: 0.5em"><input type="submit" value="Get Differences"></div>
+</form>
+<hr><address>Questions? Ask the <a href="/mailto.html">ACAA Technical Agent</a><br></address>
+</BODY></HTML>
