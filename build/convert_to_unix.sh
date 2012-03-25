259785696da2f7e43bbd70a86b11ffac6d9610fd
stephen_leake-2 <stephen_leake-2@stephe-leake.org>
2012-03-25 14:19:11 +0000

    change some files to DOS ending to minimize diff with upstream
    * build/Makefile: improve upstream fetch
    
    * build/convert_to_unix.sh: add comment about not using dos2unix
    
    * progs/arm_db.adb:
    * progs/arm_frm.adb:
    * progs/arm_frm.ads:
    * progs/arm_indx.adb: change to DOS ending to minimize diff with upstream

--- build/convert_to_unix.sh
+++ build/convert_to_unix.sh
@@ -1,1 +1,3 @@
+# 'dos2unix' is installed in Cygwin but not Debian (it could be
+# installed in Debian, but we don't need to fix what isn't broken).
 for file in *.MSS *.mss *.MSM; do  awk '{ sub("\r$", ""); print }' $file > temp; mv temp $file; done
