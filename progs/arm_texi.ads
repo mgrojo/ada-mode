c760aa6c38d2b6d621a6a792e1ab6bb1e0f3a571
stephen_leake-2 <stephen_leake-2@stephe-leake.org>
2012-04-09 11:17:56 +0000

    Update to upstream
    * progs/arm_texi.ads: New file.

---
+++ progs/arm_texi.ads
@@ -0,0 +1,45 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<HTML>
+<TITLE>CVS log for arm/progs/arm_texi.ads</TITLE>
+<h1><font color="#008000">CVS log for arm/progs/arm_texi.ads</font></h1>
+<div style="font-size: 120%; margin-top: 1.0em; margin-bottom: 0.5em">
+<img src="/icons/back.gif" style="float: left">
+<a href="/cgi-bin/cvsweb.cgi/arm/progs/">to arm/progs/</a></div>
+<div style="margin-top: 0.5em; margin-bottom: 0.7em">
+<a href="#diff">Request differences between arbitrary revisions</a></div>
+<hr noshade>
+<span style="font-size: 120%"><b><a href="/cgi-bin/cvsweb.cgi/arm/progs/arm_texi.ads?rev=1.2">1.2</a></b></span><br>
+Modified <i>2012/04/03 22:10:50</i>
+by <i>randy</i> with line changes <i>+14 -12</i><br>
+CVS Tags: <b>HEAD, Ada2012_D17</b><br>
+<a href="/cgi-bin/cvsweb.cgi/arm/progs/arm_texi.ads?diffbase=1.1&rev=1.2">Differences from 1.1</a><br>
+<div style="font-family: sans-serif; margin-top: 1.0em; margin-bottom: 0.5em">
+Various changes noted by Stephen Leake, plus a new version of the Texinfo package.
+</div><hr noshade>
+<span style="font-size: 120%"><b><a href="/cgi-bin/cvsweb.cgi/arm/progs/arm_texi.ads?rev=1.1">1.1</a></b></span><br>
+Modified <i>2012/04/03 20:56:16</i>
+by <i>randy</i><br>
+<div style="font-family: sans-serif; margin-top: 1.0em; margin-bottom: 0.5em">
+&quot;Original&quot; version of TextInfo files.
+</div>
+<hr noshade>
+<p><a name="diff">This form</a> allows you to request differences between any
+two revisions of a file, by number or by tag.</p>
+<form method="GET" action="/cgi-bin/cvsweb.cgi/arm/progs/arm_texi.ads">
+Differences between
+<select name="diffbase">
+<option selected value="1.2">1.2
+<option value="1.1">1.1
+<option value="1.2">HEAD
+<option value="1.2">Ada2012_D17
+</select>
+<select name="rev">
+<option selected value="1.2">1.2
+<option value="1.1">1.1
+<option value="1.2">HEAD
+<option value="1.2">Ada2012_D17
+</select>
+<div style="margin-top: 0.5em"><input type="submit" value="Get Differences"></div>
+</form>
+<hr><address>Questions? Ask the <a href="/mailto.html">ACAA Technical Agent</a><br></address>
+</BODY></HTML>
