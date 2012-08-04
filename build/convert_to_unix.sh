e11637374458848307dc676794fbced16da8a28e
stephen_leake-2 <stephen_leake-2@stephe-leake.org>
2012-08-04 15:26:10 +0000

    * build/Makefile: improve
    * build/convert_to_unix.sh: deleted
    
    * build/source_2005.diff: regenerate
    
    * progs/arm_html.adb: fix compile errors

--- build/convert_to_unix.sh
+++
@@ -1,3 +0,0 @@
-# 'dos2unix' is installed in Cygwin but not Debian (it could be
-# installed in Debian, but we don't need to fix what isn't broken).
-for file in *.MSS *.mss *.MSM; do  awk '{ sub("\r$", ""); print }' $file > temp; mv temp $file; done
