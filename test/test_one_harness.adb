cbbd04b756aed12255c43c12148460d36ef128be
stephen_leake-2 <stephen_leake-2@stephe-leake.org>
2012-12-22 14:30:10 +0000

    * Test/test_one_harness.adb (AUnit.Reporter.Text): change to adacore aunit 3.4.2

--- test/test_one_harness.adb
+++ test/test_one_harness.adb
@@ -2,7 +2,7 @@
 --
 --  Run one OpenToken AUnit test
 --
---  Copyright (C) 2009, 2010 Stephen Leake.  All Rights Reserved.
+--  Copyright (C) 2009, 2010, 2012 Stephen Leake.  All Rights Reserved.
 --
 --  This program is free software; you can redistribute it and/or
 --  modify it under terms of the GNU General Public License as
@@ -17,24 +17,22 @@
 --  MA 02111-1307, USA.
 
 with AUnit.Reporter.Text;
+with AUnit.Options;
 with AUnit.Test_Results;
 with AUnit.Test_Suites; use AUnit.Test_Suites;
-with Test_Token_Identifier_Real_String;
+with Lookahead_Test;
 procedure Test_One_Harness
 is
-   Suite  : constant Access_Test_Suite := new Test_Suite;
-   Result : constant AUnit.Test_Results.Result_Access := new AUnit.Test_Results.Result;
-   Status : AUnit.Status;
-   pragma Unreferenced (Status);
-   Engine : AUnit.Reporter.Text.Text_Reporter;
-
+   Suite    : constant Access_Test_Suite := new Test_Suite;
+   Reporter : AUnit.Reporter.Text.Text_Reporter;
+   Result   : AUnit.Test_Results.Result;
+   Status   : AUnit.Status;
 begin
-   Add_Test (Suite, new Test_Token_Identifier_Real_String.Test_Case (Debug => True));
+   Add_Test (Suite, new Lookahead_Test.Test_Case (Debug => False));
 
-
-   Run (Suite, Result, Status);
+   Run (Suite, AUnit.Options.Default_Options, Result, Status);
 
    --  Provide command line option -v to set verbose mode
-   AUnit.Reporter.Text.Report (Engine, Result.all);
+   AUnit.Reporter.Text.Report (Reporter, Result);
 
 end Test_One_Harness;
