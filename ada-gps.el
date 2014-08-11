e8cc0336a3e1b9d0f9e2438e2e557023143abef9
stephen_leake-2 <stephen_leake-2@stephe-leake.org>
2014-08-11 20:57:05 +0000

    ada-gps choose ada-gps or ada-wisi
    * README: clean up
    
    * ada-gps.el (ada-gps-size-threshold): new
      (ada-gps-indent-line): fix bug
      (ada-gps-or-wisi-setup): new
    
    * ada-mode.texi (Installation): add gpr_query, ada_mode_gps_indent
      (Other cross-reference): add gpr_query
      (Other indentation): new
    
    * build/Makefile (install): improve
    
    * build/run-indent-test-gps.el: New file.
    
    * build/wisi/Makefile (test-ada-gps): new
    
    * gpr-mode.texi: fix bug

--- ada-gps.el
+++ ada-gps.el
@@ -22,6 +22,12 @@
 ;; You should have received a copy of the GNU General Public License
 ;; along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
 
+(defcustom ada-gps-size-threshold 100000
+  "Max size (in characters) for using ada-wisi indentation engine."
+  :type 'integer
+  :group 'ada-indentation
+  :safe 'integerp)
+
 (defvar ada-gps-debug 0)
 
 ;;;;; sessions
@@ -115,7 +121,8 @@ If PREFIX is non-nil, prefix with count of bytes in cmd."
   ;; command string (in particular, for 'compute_indent').
   (let* ((byte-count-img (when prefix (format "%02d" (length cmd))))
 	 (msg (concat byte-count-img cmd)))
-    (when (> ada-gps-debug 0)
+    (when (and (> ada-gps-debug 0)
+	       (< (length msg) 100))
       (message msg))
     (with-current-buffer (ada-gps--session-buffer ada-gps-session)
       (erase-buffer)
@@ -153,9 +160,11 @@ If PREFIX is non-nil, prefix with count of bytes in cmd."
       )
     (with-current-buffer (ada-gps--session-buffer ada-gps-session)
       (goto-char (point-min))
-      (if (looking-at (concat " *\\([0-9+]\\)$"))
+      (if (looking-at (concat " *\\([0-9]+\\)$"))
 	  (setq indent (string-to-number (match-string 1)))
 	;; gps did not compute indent for some reason
+	(when (> ada-gps-debug 0)
+	  (message "ada-gps returned '%s'" (buffer-substring-no-properties (point-min) (point-max))))
 	(setq indent 0))
       )
 
@@ -178,8 +187,36 @@ If PREFIX is non-nil, prefix with count of bytes in cmd."
   ;; FIXME: fontify 'return' (see ada-wisi-post-local-vars)
   )
 
-;; FIXME: make this per-buffer, depending on size threshold
-(add-hook 'ada-mode-hook 'ada-gps-setup)
+(require 'ada-wisi)
+
+(defun ada-gps-or-wisi-setup ()
+  "If buffer size > `ada-gps-size-threshold', use ada-gps; otherwise use ada-wisi indentation engine,"
+  (if (> (point-max) ada-gps-size-threshold)
+      (progn
+	(ada-gps-setup)
+
+	;; locally clear global function pointers set by loading ada-wisi
+	(set (make-local-variable 'ada-fix-context-clause) nil)
+	(set (make-local-variable 'ada-goto-declaration-start) nil)
+	(set (make-local-variable 'ada-goto-declaration-end) nil)
+	(set (make-local-variable 'ada-goto-declarative-region-start) nil)
+	(set (make-local-variable 'ada-goto-end) nil)
+	(set (make-local-variable 'ada-in-paramlist-p) nil)
+	(set (make-local-variable 'ada-indent-statement) nil)
+	(set (make-local-variable 'ada-make-subprogram-body) nil)
+	(set (make-local-variable 'ada-next-statement-keyword) nil)
+	(set (make-local-variable 'ada-prev-statement-keyword) nil)
+	(set (make-local-variable 'ada-reset-parser) nil)
+	(set (make-local-variable 'ada-scan-paramlist) nil)
+	(set (make-local-variable 'ada-show-parse-error) nil)
+	(set (make-local-variable 'ada-which-function) nil)
+	)
+
+    (ada-wisi-setup))
+  )
+
+(add-hook 'ada-mode-hook 'ada-gps-or-wisi-setup)
+(setq ada-mode-hook (delq 'ada-wisi-setup ada-mode-hook))
 
 (provide 'ada-gps)
 (provide 'ada-indent-engine)
