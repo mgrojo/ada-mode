e7623bb91364fc6c5d6c2d8f6a89672e24c7f36a
stephen_leake-2 <stephen_leake-2@stephe-leake.org>
2016-02-11 18:35:37 +0000

    * source/aunit-checks-containers.ad?: New file.

---
+++ source/aunit-checks-containers.adb
@@ -0,0 +1,54 @@
+--  Abstract :
+--
+--  See spec.
+--
+--  Copyright (C) 2016  All Rights Reserved.
+--
+--  This program is free software; you can redistribute it and/or
+--  modify it under terms of the GNU General Public License as
+--  published by the Free Software Foundation; either version 3, or (at
+--  your option) any later version. This program is distributed in the
+--  hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+--  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+--  PURPOSE. See the GNU General Public License for more details. You
+--  should have received a copy of the GNU General Public License
+--  distributed with this program; see file COPYING. If not, write to
+--  the Free Software Foundation, 51 Franklin Street, Suite 500, Boston,
+--  MA 02110-1335, USA.
+--
+--  As a special exception, if other files instantiate generics from
+--  this unit, or you link this unit with other files to produce an
+--  executable, this  unit  does not  by itself cause  the resulting
+--  executable to be covered by the GNU General Public License. This
+--  exception does not however invalidate any other reasons why the
+--  executable file  might be covered by the  GNU Public License.
+
+pragma License (GPL);
+
+package body AUnit.Checks.Containers is
+
+   procedure Gen_Check_Indefinite_Vector
+     (Label          : in String;
+      Computed       : in Container_Pkg.Vector;
+      Expected       : in Container_Pkg.Vector;
+      Strict_Indices : in Boolean := True)
+   is
+      J : Index_Type := Expected.First_Index;
+   begin
+      if Strict_Indices then
+         Check_Index (Label & ".First", Computed.First_Index, Expected.First_Index);
+         Check_Index (Label & ".Last", Computed.Last_Index, Expected.Last_Index);
+      else
+         Check (Label & ".Length", Computed.Length, Expected.Length);
+      end if;
+
+      for I in Computed.First_Index .. Computed.Last_Index loop
+         Check_Element (Label & "." & Index_Type'Image (I), Computed (I), Expected (J));
+         if J /= Index_Type'Last then
+            J := Index_Type'Succ (J);
+         end if;
+      end loop;
+      null;
+   end Gen_Check_Indefinite_Vector;
+
+end AUnit.Checks.Containers;
---
+++ source/aunit-checks-containers.ads
@@ -0,0 +1,53 @@
+--  Abstract :
+--
+--  Checks for Ada.Containers
+--
+--  Copyright (C) 2016  All Rights Reserved.
+--
+--  This program is free software; you can redistribute it and/or
+--  modify it under terms of the GNU General Public License as
+--  published by the Free Software Foundation; either version 3, or (at
+--  your option) any later version. This program is distributed in the
+--  hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+--  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+--  PURPOSE. See the GNU General Public License for more details. You
+--  should have received a copy of the GNU General Public License
+--  distributed with this program; see file COPYING. If not, write to
+--  the Free Software Foundation, 51 Franklin Street, Suite 500, Boston,
+--  MA 02110-1335, USA.
+--
+--  As a special exception, if other files instantiate generics from
+--  this unit, or you link this unit with other files to produce an
+--  executable, this  unit  does not  by itself cause  the resulting
+--  executable to be covered by the GNU General Public License. This
+--  exception does not however invalidate any other reasons why the
+--  executable file  might be covered by the  GNU Public License.
+
+pragma License (GPL);
+
+with Ada.Containers.Indefinite_Vectors;
+package AUnit.Checks.Containers is
+
+   procedure Check is new Gen_Check_Discrete (Ada.Containers.Count_Type);
+
+   generic
+      type Element_Type (<>) is private;
+      type Index_Type is range <>;
+      with package Container_Pkg is new Ada.Containers.Indefinite_Vectors
+        (Index_Type   => Index_Type,
+         Element_Type => Element_Type);
+      with procedure Check_Index
+        (Label    : in String;
+         Computed : in Index_Type;
+         Expected : in Index_Type);
+      with procedure Check_Element
+        (Label    : in String;
+         Computed : in Element_Type;
+         Expected : in Element_Type);
+   procedure Gen_Check_Indefinite_Vector
+     (Label          : in String;
+      Computed       : in Container_Pkg.Vector;
+      Expected       : in Container_Pkg.Vector;
+      Strict_Indices : in Boolean := True);
+
+end AUnit.Checks.Containers;
