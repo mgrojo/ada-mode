ab3469dc87617614d8d0ead712c88101186b774e
stephen_leake-2 <stephen_leake-2@stephe-leake.org>
2017-01-16 21:21:09 +0000

    * ada_mode_gps_indent.adb (Read_Input): new; workaround for gnat.os_lib.read not waiting for all N bytes. (Get_Command_Length, Ada_Mode_GPS_Indent): use Read_input
    * build/Makefile: clean up comment

--- ada_mode_gps_indent.adb
+++ ada_mode_gps_indent.adb
@@ -3,7 +3,7 @@
 --  Back end for Emacs Ada mode indentation engine, using GPS
 --  indentation code.
 --
---  Copyright (C) 2014  All Rights Reserved.
+--  Copyright (C) 2014, 2017  All Rights Reserved.
 --
 --  This program is free software; you can redistribute it and/or
 --  modify it under terms of the GNU General Public License as
@@ -30,6 +30,7 @@ with GNAT.OS_Lib;
 with GNAT.Traceback.Symbolic;
 with GNATCOLL.Symbols;
 with Language;
+with System.Storage_Elements;
 procedure Ada_Mode_GPS_Indent is
 
    Programmer_Error : exception;
@@ -57,16 +58,41 @@ procedure Ada_Mode_GPS_Indent is
 
    end Usage;
 
+   procedure Read_Input
+     (A : System.Address;
+      N : Integer)
+   is
+      use System.Storage_Elements;
+
+      B         : System.Address := A;
+      Remaining : Integer        := N;
+      Read      : Integer;
+   begin
+      --  WORKAROUND: with GNAT GPL 2016, GNAT.OS_Lib.Read does _not_
+      --  wait for all N bytes or EOF; it returns as soon as it gets
+      --  some bytes.
+      --
+      --  Note that with this loop we cannot detect EOF; that's ok for
+      --  this application.
+      loop
+         Read := GNAT.OS_Lib.Read (GNAT.OS_Lib.Standin, B, Remaining);
+         Remaining := Remaining - Read;
+         exit when Remaining <= 0;
+         B := B + Storage_Offset (Read);
+      end loop;
+   end Read_Input;
+
    function Get_Command_Length return Integer
    is
       Temp : aliased String (1 .. 2) := "  ";
-      Read_Bytes : constant Integer := GNAT.OS_Lib.Read (GNAT.OS_Lib.Standin, Temp'Address, 2);
    begin
-      if Read_Bytes /= 2 then
-         raise Programmer_Error with "2 bytes of command byte count not provided; got" &
-           Integer'Image (Read_Bytes) & "'" & Temp & "'";
-      end if;
+      Read_Input (Temp'Address, 2);
       return Integer'Value (Temp);
+
+   exception
+   when Constraint_Error =>
+      --  From Integer'Value
+      raise Programmer_Error with "command byte count not provided; got '" & Temp & "'";
    end Get_Command_Length;
 
    function Get_Integer
@@ -124,14 +150,10 @@ begin
          use Ada.Strings.Fixed;
          Command_Length : constant Integer := Get_Command_Length;
          Command_Line   : aliased String (1 .. Command_Length);
-
-         Read_Bytes : Integer := GNAT.OS_Lib.Read (GNAT.OS_Lib.Standin, Command_Line'Address, Command_Length);
-         Last       : Integer := Index (Source => Command_Line, Pattern => " ");
+         Last           : Integer;
       begin
-         if Read_Bytes /= Command_Length then
-            raise Programmer_Error with
-              "Read_Bytes" & Integer'Image (Read_Bytes) & " /= Command_Length" & Integer'Image (Command_Length);
-         end if;
+         Read_Input (Command_Line'Address, Command_Length);
+         Last := Index (Source => Command_Line, Pattern => " ");
 
          if Last = 0 then
             Last := Command_Line'Last;
@@ -150,46 +172,43 @@ begin
                Byte_Count  : constant Integer      := Get_Integer (Command_Line, Last);
                Buffer      : aliased String_Access := new String (1 .. Byte_Count);
             begin
-               Read_Bytes := GNAT.OS_Lib.Read (GNAT.OS_Lib.Standin, Buffer.all'Address, Byte_Count);
+               Read_Input (Buffer.all'Address, Byte_Count);
 
-               if Read_Bytes /= Byte_Count then
-                  Put_Line ("Read_Bytes" & Integer'Image (Read_Bytes) & " /= Byte_Count" & Integer'Image (Byte_Count));
-               else
-                  Ada_Analyzer.Analyze_Ada_Source
-                    (Buffer.all, GNATCOLL.Symbols.Allocate,
-                     Indent_Params          =>
-                       (Indent_Level        => 3, -- FIXME: get from ada-mode
-                        Indent_Continue     => 2,
-                        Indent_Decl         => 2,
-                        Indent_Conditional  => 1,
-                        Indent_Record       => 3,
-                        Indent_Case_Extra   => Language.Automatic,
-                        Casing_Policy       => Case_Handling.Disabled,
-                        Reserved_Casing     => Case_Handling.Unchanged,
-                        Ident_Casing        => Case_Handling.Unchanged,
-                        Format_Operators    => True,
-                        Use_Tabs            => False,
-                        Align_On_Colons     => True, -- FIXME: get from ada-mdoe
-                        Align_On_Arrows     => True,
-                        Align_Decl_On_Colon => False,
-                        Indent_Comments     => True,
-                        Stick_Comments      => False),
-                     From                   => Indent_Line,
-                     To                     => Indent_Line,
-                     Replace                => Replace_Cb'Unrestricted_Access);
-               end if;
+               Ada_Analyzer.Analyze_Ada_Source
+                 (Buffer.all, GNATCOLL.Symbols.Allocate,
+                  Indent_Params          =>
+                    (Indent_Level        => 3, -- FIXME: get from ada-mode
+                     Indent_Continue     => 2,
+                     Indent_Decl         => 2,
+                     Indent_Conditional  => 1,
+                     Indent_Record       => 3,
+                     Indent_Case_Extra   => Language.Automatic,
+                     Casing_Policy       => Case_Handling.Disabled,
+                     Reserved_Casing     => Case_Handling.Unchanged,
+                     Ident_Casing        => Case_Handling.Unchanged,
+                     Format_Operators    => True,
+                     Use_Tabs            => False,
+                     Align_On_Colons     => True, -- FIXME: get from ada-mdoe
+                     Align_On_Arrows     => True,
+                     Align_Decl_On_Colon => False,
+                     Indent_Comments     => True,
+                     Stick_Comments      => False),
+                  From                   => Indent_Line,
+                  To                     => Indent_Line,
+                  Replace                => Replace_Cb'Unrestricted_Access);
+
                Free (Buffer);
             exception
-               when E : others =>
-                  declare
-                     use Ada.Exceptions;
-                     use GNAT.Traceback.Symbolic;
-                  begin
-                     Put_Line ("analyze failed on '" & Buffer.all & "'");
-                     Put_Line ("Exception : " & Exception_Name (E));
-                     Put_Line (Exception_Message (E));
-                     Put_Line (Symbolic_Traceback (E));
-                  end;
+            when E : others =>
+               declare
+                  use Ada.Exceptions;
+                  use GNAT.Traceback.Symbolic;
+               begin
+                  Put_Line ("analyze failed on '" & Buffer.all & "'");
+                  Put_Line ("Exception : " & Exception_Name (E));
+                  Put_Line (Exception_Message (E));
+                  Put_Line (Symbolic_Traceback (E));
+               end;
             end;
 
          else
