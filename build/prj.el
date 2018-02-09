e8d12effd6e94651221cba5777aaf5b67b89970c
stephen_leake-2 <stephen_leake-2@stephe-leake.org>
2018-02-09 00:19:44 +0000

    * build/prj.el (prj-file): fix prj-name

--- build/prj.el
+++ build/prj.el
@@ -6,7 +6,7 @@
 (add-to-list 'project-find-functions 'project-menu-prj)
 
 (let* ((prj-file (expand-file-name "sal.prj"))
-       (prj-name "sal main")
+       (prj-name "sal stephe-1")
        (prj (make-ada-project
 	     :env-vars nil
 	     :ada-prj-file prj-file)))
