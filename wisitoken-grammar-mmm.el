4468a4dafcd9e6143c4301caa4a670b207c3918b
stephen_leake-2 <stephen_leake-2@stephe-leake.org>
2019-04-10 05:59:42 +0000

    * wisitoken-grammar-mmm.el: add wisi-code
    * wisitoken_grammar_1.wy (declaration): add face on '%code'

--- wisitoken-grammar-mmm.el
+++ wisitoken-grammar-mmm.el
@@ -9,6 +9,12 @@
     :front "%("
     :back ")%"
     :insert ((?a wisi-action nil @ "%(" @ "" _ "" @ ")%")))
+   (wisi-code
+    :match-submode wisi-mmm-submode
+    :face mmm-code-submode-face
+    :front "%{"
+    :back "}%"
+    :insert ((?a wisi-code nil @ "%{" @ "" _ "" @ "}%")))
    ))
 
 (defvar wisitoken-grammar-action-mode) ;; in wisitoken-grammar-mode.el
@@ -17,6 +23,7 @@
   wisitoken-grammar-action-mode)
 
 (add-to-list 'mmm-mode-ext-classes-alist '(wisitoken-grammar-mode nil wisi-action))
+(add-to-list 'mmm-mode-ext-classes-alist '(wisitoken-grammar-mode nil wisi-code))
 
 (provide 'wisitoken-grammar-mmm)
 ;;; end of file
