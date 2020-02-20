51dd527845b92615411b4c96c9ebd7f89e4c3f17
Stephen Leake <stephen_leake@stephe-leake.org>
2020-02-20 11:28:56 -0800

    * .mtn-ignore: Delete
    * source/sal-gen_bounded_definite_vectors_sorted.adb (Insert): add annotations to allow spark proof
    
    * source/sal-gen_bounded_definite_vectors_sorted.ads (Is_Sorted): rewrite annotations to allow spark proof

--- source/sal-gen_bounded_definite_vectors_sorted.ads
+++ source/sal-gen_bounded_definite_vectors_sorted.ads
@@ -51,40 +51,50 @@ is
      Pre => Index in First_Index (Container) .. Last_Index (Container);
 
    function Is_Sorted (Container : in Vector) return Boolean is
+     --  See comment on similar Is_Sorted below
      (for all I in First_Index (Container) .. Last_Index (Container) - 1 =>
-        Element_Compare (Element (Container, I), Element (Container, I + 1)) in Less | Equal);
+        (for all J in I + 1 .. Last_Index (Container) =>
+           Element_Compare (Element (Container, I), Element (Container, J)) in Less | Equal));
 
    procedure Insert
      (Container       : in out Vector;
       New_Item        : in     Element_Type;
       Ignore_If_Equal : in     Boolean := False) with
-     Pre  => Length (Container) < Capacity,
+     Pre  => Last_Index (Container) < Peek_Type (Capacity),
      Post => Is_Sorted (Container) and
-             (Last_Index (Container) = Last_Index (Container'Old) or
-              Last_Index (Container) = Last_Index (Container'Old) + 1);
+             (if Ignore_If_Equal then
+                (Last_Index (Container) = Last_Index (Container'Old) or
+                 Last_Index (Container) = Last_Index (Container'Old) + 1)
+              else
+                Last_Index (Container) = Last_Index (Container'Old) + 1);
    --  Insert New_Item in sorted position. Items are sorted in increasing
    --  order according to Element_Compare. New_Item is inserted after
    --  Equal items, unless Ignore_If_Equal is true, in which case
    --  New_Item is not inserted.
-   --
-   --  The presense of Ignore_If_Equal makes it too difficult to prove
-   --  whether the length did or did not increase.
 
 private
 
    type Array_Type is array (Peek_Type range 1 .. Peek_Type (Capacity)) of aliased Element_Type;
 
    function Is_Sorted (Container : in Array_Type; Last : in Base_Peek_Type) return Boolean
+     --  This is too hard for gnatprove (in 2019):
+     --  is (for all I in Container'First .. Last - 1 =>
+     --        Element_Compare (Container (I), Container (I + 1)) in Less | Equal)
+     --  This works:
      is (for all I in Container'First .. Last - 1 =>
-           Element_Compare (Container (I), Container (I + 1)) in Less | Equal)
+           (for all J in I + 1 .. Last =>
+              Element_Compare (Container (I), Container (J)) in Less | Equal))
      with Pre => Last <= Container'Last;
+
+   subtype Index_Type is Base_Peek_Type range No_Index .. Base_Peek_Type (Capacity);
+   --  Helps with proofs
 
    type Vector is record
       Elements : Array_Type;
-      Last     : Base_Peek_Type := No_Index;
+      Last     : Index_Type := No_Index;
    end record with
      Type_Invariant => Last <= Elements'Last and Is_Sorted (Vector.Elements, Vector.Last);
    pragma Annotate (GNATprove, Intentional, "type ""Vector"" is not fully initialized",
-                    "Only items in Elements with index < Last are accessed");
+                    "Only items in Elements with index <= Last are accessed");
 
 end SAL.Gen_Bounded_Definite_Vectors_Sorted;
