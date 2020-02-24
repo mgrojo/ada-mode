cb4469ceb96a4be7d5c70fd974dde16516ac531b
Stephen Leake <stephen_leake@stephe-leake.org>
2020-02-24 15:41:47 -0800

    * ada-process.el:update * ada_re2c.c: * ada_re2c_c.ads:
    * build/create_makefile_conf.sh: don't need HAVE_GNATCOLL_XREF any more
    
    * wisitoken-parse-lr-mckenzie_recover-ada.adb: restore
    * wisitoken-parse-lr-mckenzie_recover-ada.ads: restore

--- build/create_makefile_conf.sh
+++ build/create_makefile_conf.sh
@@ -1,12 +1,4 @@
-# Create Makefile.conf, which defines HAVE_GNAT_XREF, HAVE_LIBADALANG
-
-echo 'with "gnatcoll_xref"; abstract project check is end check;' > check.gpr
-gprbuild -P check.gpr > /dev/null 2>&1
-if test $? -eq 0 ; then
-    echo 'HAVE_GNATCOLL_XREF := "yes"' > Makefile.conf
-else
-    echo 'HAVE_GNATCOLL_XREF := "no"' > Makefile.conf
-fi
+# Create Makefile.conf, which defines HAVE_LIBADALANG
 
 echo 'with "libadalang"; abstract project check is end check;' > check.gpr
 gprbuild -P check.gpr > /dev/null 2>&1
