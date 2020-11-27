9227780a6541247d0d9ad54d1838a5c8b10104ab
Stephen Leake <stephen_leake@stephe-leake.org>
2020-11-27 10:59:25 -0800

    resolve conflicts, finish propagate

--- build/prj.el
+++ build/prj.el
@@ -1,19 +1,18 @@
 ;; Project definitions for compiling ada-mode
 
-(let ((wisitoken-core "/Projects/org.wisitoken"))
+(let ((wisitoken-core "/Projects/org.wisitoken.stephe-2"))
 
   (wisi-prj-select-cache
    "ada_mode_wisi_parse.prj"
    (create-ada-prj
-    :name "ada_mode_wisi_parse main"
+    :name "ada_mode_wisi_parse stephe-6"
     :compile-env
     (append
-     (list "SAL=../../org.stephe_leake.sal")
+     (list "SAL=../../org.stephe_leake.sal.stephe-1")
      (cl-ecase system-type
       (gnu/linux
        (list
-	(concat "WISITOKEN=" wisitoken-core)
-	"LIBADALANG=/Projects/libadalang/build/lib/gnat"))
+	(concat "WISITOKEN=" wisitoken-core)))
       (windows-nt
        (list
 	(concat "WISITOKEN=c:" wisitoken-core))))
