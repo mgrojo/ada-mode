68564a24e06b4cb19ba0e4d3df256a43a3f0969d
Stephen Leake <stephen_leake@stephe-leake.org>
2022-03-10 18:58:33 -0800

    Report memory after creating parser
    * gen_run_wisi_lr_text_rep_parse.adb (Gen_Run_Wisi_LR_Text_Rep_Parse):
    Report memory after creating parser.

--- gen_run_wisi_lr_text_rep_parse.adb
+++ gen_run_wisi_lr_text_rep_parse.adb
@@ -20,6 +20,7 @@ pragma License (GPL);
 
 with Ada.Command_Line;
 with Ada.Directories;
+with GNATCOLL.Memory;
 with Run_Wisi_Common_Parse;
 with WisiToken.Text_IO_Trace;
 procedure Gen_Run_Wisi_LR_Text_Rep_Parse
@@ -27,10 +28,26 @@ is
    Trace               : aliased WisiToken.Text_IO_Trace.Trace;
    Parse_Data_Template : aliased Parse_Data_Type;
 begin
-   Run_Wisi_Common_Parse.Parse_File
-     ((Descriptor, Create_Lexer (Trace'Unchecked_Access), Create_Parse_Table
-         (Ada.Directories.Containing_Directory (Ada.Command_Line.Command_Name) & "/" & Text_Rep_File_Name),
-       Partial_Parse_Active, Partial_Parse_Byte_Goal, Language_Fixes, Language_Matching_Begin_Tokens,
-       Language_String_ID_Set, Parse_Data_Template'Unchecked_Access),
-      Trace'Unchecked_Access);
+   --  FIXME: report memory during lexer, parser create
+   --  WisiToken.Trace_Memory            := 1;
+   --  WisiToken.Trace_Incremental_Parse := 1;
+   GNATCOLL.Memory.Configure
+     (Activate_Monitor      => True,
+      Stack_Trace_Depth     => 0,
+      Reset_Content_On_Free => False);
+
+   declare
+      Lexer : constant WisiToken.Lexer.Handle := Create_Lexer (Trace'Unchecked_Access);
+      --  No point in reporting lexer memory; it's very small
+      Parse_Table : constant WisiToken.Parse.LR.Parse_Table_Ptr := Create_Parse_Table
+        (Ada.Directories.Containing_Directory (Ada.Command_Line.Command_Name) & "/" & Text_Rep_File_Name);
+   begin
+      Trace.Put_Line ("parse table created");
+      WisiToken.Report_Memory (Trace);
+
+      Run_Wisi_Common_Parse.Parse_File
+        ((Descriptor, Lexer, Parse_Table, Partial_Parse_Active, Partial_Parse_Byte_Goal,
+          Language_Fixes, Language_Matching_Begin_Tokens, Language_String_ID_Set, Parse_Data_Template'Unchecked_Access),
+         Trace'Unchecked_Access);
+   end;
 end Gen_Run_Wisi_LR_Text_Rep_Parse;
