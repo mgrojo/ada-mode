a1d7eaa67f3a01738db4df4ed19049aeb81c8e44
Stephen Leake <stephen_leake@stephe-leake.org>
2022-07-09 10:23:48 -0700

    Misc improvements, preparing for release
    * benchmark_results.text: Update.
    
    * build.sh (WISI_DIR): Generate lr1 table file.
    
    * build/Makefile (one, two): Improve.
    
    * build/benchmark.el (wisi-parser-shared): Match wisi change.
    
    * build/exclude-elpa.el: Also disable native compilation.

--- build/exclude-elpa.el
+++ build/exclude-elpa.el
@@ -2,4 +2,9 @@
 (defvar package-load-list '(all))
 (push '(ada-mode . nil) package-load-list)
 (push '(wisi . nil) package-load-list)
+
+;; also disable native compilation; it just slows things down
+;; (defun native-comp-available-p () nil)
+(setq native-comp-deferred-compilation nil)
+
 ;; end of file
