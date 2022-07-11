e8079f3796b27ee12defd07cc1b01a45a341160d
Stephen Leake <stephen_leake@stephe-leake.org>
2022-07-11 13:51:40 -0700

    Misc fixes for beta release
    * Makefile (all): Add byte-compile.
    
    * NEWS: Beta release.
    
    * ada-build.el: Fix quotes in doc strings.
    * ada-indent-user-options.el:
    * gpr-query.el:
    
    * ada-gnat-xref.el: Fix cl-defgeneric args.
    
    * ada-mode.el (ada-mode-version): Beta version.
    
    * ada_mode-indent_02.adb: Delete.
    
    * benchmark-xref.el: Add copyright.
    (benchmark-xref): Fix quotes in doc string.
    
    * build.sh: Improve verbosity, fix "try devel version". Invode gprbuild
    directly for wisitoken-bnf-generate. Don't unzip the parse table.
    
    * build/Makefile (all): Add docs.
    (pub): Remove docs.
    (pub-ada): Update.
    
    * build/exclude-elpa.el: Use native-comp-speed to disable native
    compilation; better documented.
    
    * build/install-elpa.el: Add pop-to-buffer messages.

--- benchmark-xref.el
+++ benchmark-xref.el
@@ -1,4 +1,22 @@
-;; xref backend agnostic benchmark for xref operations
+;;; benchmark-xref.el --- xref backend agnostic benchmark for xref operations
+;;
+;; Copyright (C) 2022  Free Software Foundation, Inc.
+;;
+;; This file is part of GNU Emacs.
+;;
+;; GNU Emacs is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; GNU Emacs is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
+;;
 
 (defun bench-xref-one-test (test xref-function)
   (find-file (car test))
@@ -16,7 +34,7 @@
 (defun benchmark-xref (test-list xref-function)
   "TEST-LIST is a list of TEST, where TEST is a list (FILENAME (TARGET ...) ...).
 For each TEST, open the file, and search for each TARGET,
-starting from point-min. If TARGET is "", start the next test
+starting from point-min. If TARGET is \"\", start the next test
 from current point instead. Then invoke xref-function. Repeat for
 each list of TARGET, using the current file. Show the time for
 each TEST, and the total time.
--- build/exclude-elpa.el
+++ build/exclude-elpa.el
@@ -4,7 +4,6 @@
 (push '(wisi . nil) package-load-list)
 
 ;; also disable native compilation; it just slows things down
-;; (defun native-comp-available-p () nil)
-(setq native-comp-deferred-compilation nil)
+(setq native-comp-speed -1)
 
 ;; end of file
