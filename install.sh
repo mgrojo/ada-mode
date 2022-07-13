22f8f42a242e7b65c0801653cc965b2aefeb0c7b
Stephen Leake <stephen_leake@stephe-leake.org>
2022-07-13 14:52:13 -0700

    Bump version to 7.3.beta1
    * ada-mode.el: Bump version to 7.3.beta1.
    
    * build/Makefile (ADA_MODE_VERSION, WISI_VERSION): Bump.
    (pub-ada): Improve.
    
    * install.sh: Current version for WISI_DIR.
    
    * notes.text: Improve release process.

--- install.sh
+++ install.sh
@@ -8,11 +8,11 @@
 # If you don't have write permission in the GNAT installation
 # directory, you need to use --prefix=<dir>, or run with root priviledges.
 
-if [ -d ../wisi-3.1.? ]; then
-    WISI_DIR=`ls -d ../wisi-3.1.?`
+if [ -d ../wisi-4.0.? ]; then
+    WISI_DIR=`ls -d ../wisi-4.0.?`
 else
     # try devel version
-    WISI_DIR=`ls -d ../wisi-3.1.?.0.*`
+    WISI_DIR=`find .. -type d -name "wisi-4.0beta*"`
 fi
 
 gprinstall -f -p -P ada_mode_wisi_parse.gpr -aP $WISI_DIR --install-name=ada_mode_wisi_parse $1 
