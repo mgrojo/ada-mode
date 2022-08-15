f609da4e1d5860a2fef1e7a3866168a4ab7281c7
Stephen Leake <stephen_leake@stephe-leake.org>
2022-08-15 14:31:26 -0700

    * build/Makefile (WISI): New
    * git_checkout.sh: Make executable.
    
    * prj-eglot.el: New file.

---
+++ prj-eglot.el
@@ -0,0 +1,12 @@
+(setq eglot-extend-to-xref t)
+
+(let ((dir (file-name-directory (or load-file-name (buffer-file-name)))))
+  (setenv "GPR_PROJECT_PATH"
+	  (concat (expand-file-name "." dir)
+		path-separator (expand-file-name "../org.emacs.wisi" dir)
+		path-separator (expand-file-name "../org.wisitoken/build" dir)
+		path-separator (expand-file-name "../org.stephe_leake.sal/build" dir)
+		path-separator (expand-file-name "../org.stephe_leake.makerules" dir)
+		path-separator (expand-file-name "../org.stephe_leake.aunit_ext/build" dir))
+	))
+;; end of file
