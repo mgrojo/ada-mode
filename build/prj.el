6be7b3961fac693a88523232c78b647fafc9d97e
Stephen Leake <stephen_leake@stephe-leake.org>
2022-08-18 11:09:59 -0700

    * build/prj.el: Improve

--- build/prj.el
+++ build/prj.el
@@ -6,33 +6,35 @@
   (add-hook 'project-find-functions #'wisi-prj-find-dominating-cached -10)
   (add-hook 'xref-backend-functions #'wisi-prj-xref-backend 10))
 
-(let* ((wisitoken-core "/Projects/org.wisitoken")
-       (wisi-core "/Projects/org.emacs.wisi")
-       (prj
-	(wisi-prj-select-cache
-	 "gpr_mode_wisi_parse.prj"
-	 (make-wisi-prj
-	  :name "gpr_mode_wisi_parse main"
-	  :compile-env
-	  (append
-	   (list "SAL=../../org.stephe_leake.sal")
-	   (cl-ecase system-type
-	     (gnu/linux
-	      (list
-	       (concat "WISITOKEN=" wisitoken-core)
-	       (concat "WISI=" wisi-core)))
-	     (windows-nt
-	      (list
-	       (concat "WISITOKEN=c:" wisitoken-core)
-	       (concat "WISI=c:" wisi-core))))
-	   )
-	  :compiler
-	  (create-gnat-compiler)) ;; sets gpr_project_path
-	 "Makefile"
-	 )))
-  (wisi-prj-set-dominating
+(let ((wisitoken-core "/Projects/org.wisitoken")
+      (wisi-core "/Projects/org.emacs.wisi"))
+  (wisi-prj-select-cache
    "gpr_mode_wisi_parse.prj"
-   (make-wisi-prj)
-   "Makefile"))
+   (make-wisi-prj
+    :name "gpr_mode_wisi_parse main"
+    :compile-env
+    (append
+     (list "SAL=../../org.stephe_leake.sal")
+     (cl-ecase system-type
+       (gnu/linux
+	(list
+	 (concat "WISITOKEN=" wisitoken-core)
+	 (concat "WISI=" wisi-core)))
+       (windows-nt
+	(list
+	 (concat "WISITOKEN=c:" wisitoken-core)
+	 (concat "WISI=c:" wisi-core))))
+     )
+    :compiler
+    (create-gnat-compiler)) ;; sets gpr_project_path
+   "Makefile"
+   ))
+
+(wisi-prj-set-dominating
+ "gpr_mode_wisi_parse.prj"
+ (make-wisi-prj
+     :compiler
+    (create-gnat-compiler))
+ "Makefile")
 
 ;; end of file
