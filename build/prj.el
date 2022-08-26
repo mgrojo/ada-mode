7647783a898542e1c8944b3b887eae066d91a731
Stephen Leake <stephen_leake@stephe-leake.org>
2022-08-26 01:55:34 -0700

    Use new gnat-compiler package
    * build/Makefile: Simplify setting WISITOKEN etc.
    (byte-compile): Add GNAT_COMPILER.
    
    * build/prj.el: Simplify.
    
    * gpr-mode.el: Change gnat-core to gnat-compiler.

--- build/prj.el
+++ build/prj.el
@@ -1,40 +1,20 @@
 ;; Project definitions for compiling gpr-mode
-(require 'wisi-prj)
-(require 'gnat-core)
 
 (unless (memq #'wisi-prj-find-dominating-cached project-find-functions)
   (add-hook 'project-find-functions #'wisi-prj-find-dominating-cached -10)
   (add-hook 'xref-backend-functions #'wisi-prj-xref-backend 10))
 
-(let ((wisitoken-core "/Projects/org.wisitoken")
-      (wisi-core "/Projects/org.emacs.wisi"))
-  (wisi-prj-select-cache
-   "gpr_mode_wisi_parse.prj"
-   (make-wisi-prj
-    :name "gpr_mode_wisi_parse main"
-    :compile-env
-    (append
-     (list "SAL=../../org.stephe_leake.sal")
-     (cl-ecase system-type
-       (gnu/linux
-	(list
-	 (concat "WISITOKEN=" wisitoken-core)
-	 (concat "WISI=" wisi-core)))
-       (windows-nt
-	(list
-	 (concat "WISITOKEN=c:" wisitoken-core)
-	 (concat "WISI=c:" wisi-core))))
-     )
-    :compiler
-    (create-gnat-compiler)) ;; sets gpr_project_path
-   "Makefile"
-   ))
-
-(wisi-prj-set-dominating
+(wisi-prj-select-cache
  "gpr_mode_wisi_parse.prj"
- (make-wisi-prj
-     :compiler
-    (create-gnat-compiler))
+ (create-ada-prj
+  :name "gpr_mode_wisi_parse main"
+  :compile-env
+  (list
+   (concat "GPR_COMPILER=" (expand-file-name "../../org.emacs.gpr-compiler"))
+   (concat "SAL=" 	   (expand-file-name "../../org.stephe_leake.sal.stephe-1"))
+   (concat "WISITOKEN="    (expand-file-name "../../org.wisitoken.stephe-1"))
+   (concat "WISI=" 	   (expand-file-name "../../org.emacs.wisi.stephe-4"))
+   ))
  "Makefile")
 
 ;; end of file
