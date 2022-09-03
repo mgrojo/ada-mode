bad5c20a6c2096fb8303158f26cb10d4796d69a1
Stephen Leake <stephen_leake@stephe-leake.org>
2022-09-03 14:00:33 -0700

    Work on building with Alire
    * build/prj-eglot.prj: New file.
    
    * build/wisitoken.prj (casing): Match file name changes; use GPR_PROJECT_PATH for casing.
    
    * prj-eglot.el: Improve.
    
    * wisitoken-parse-tree_sitter.adb: Handle Error_Token.
    
    * wisitoken-parse-tree_sitter.ads: Change Error_Token_ID to
    C_Error_Token_ID; value is not in range of Token_ID.
    
    * wisitoken-productions.ads (Right_Hand_Side): Explicitly init *

--- prj-eglot.el
+++ prj-eglot.el
@@ -1,13 +1,30 @@
 ;; Set up building with Alire, editing with mixed ada-mode and eglot.
 
+(require 'ada-mode)
+;; This require is not needed for the following code, but is needed to
+;; ensure ada-mode-hook has sal-ada-mode-setup.
+
+(setq ada-indent-engine 'eglot) ;; testing eglot indent
+
+(setq ada-xref-tool 'eglot) ;; testing wisi-xref lsp backend
+
+(add-hook 'ada-mode-hook #'ada-eglot-setup)
+
+;; No lanuage server for gpr, so use wisi parser for that.
+
+;; For Ada, use as many eglot features as possible
+
 (let* ((gpr-file (expand-file-name "build/wisitoken_alire.gpr" (file-name-directory load-file-name)))
+       (prj-file (expand-file-name "build/prj-eglot.prj" (file-name-directory load-file-name)))
        (eglot-workspace-configuration (list `(ada (projectFile . ,gpr-file))))
+
        (project
 	(create-alire-project
 	 :prj-name "wisitoken stephe-3 Alire eglot"
+	 :prj-file prj-file
 	 :gpr-file gpr-file)))
 
-  (wisi-prj-select-cache load-file-name project)
+  (wisi-prj-select-cache prj-file nil "Makefile")
 
   ;; ada_language_server gets GPR_PROJECT_PATH from its process environment.
   (let ((process-environment
