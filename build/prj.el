332ff2b678404f87bb54ba127ebb98b58e83781b
Stephen Leake <stephen_leake@stephe-leake.org>
2022-09-09 13:42:04 -0700

    * build/prj.el: Improve

--- build/prj.el
+++ build/prj.el
@@ -1,30 +1,18 @@
 ;; assumes wisi autoloads are defined.
 
-(when (memq #'project-try-vc project-find-functions)
-  (remove-hook 'project-find-functions #'project-try-vc))
-
-(unless (memq #'wisi-prj-current-cached project-find-functions)
-  (add-hook 'project-find-functions #'wisi-prj-current-cached 90)
-  (add-hook 'xref-backend-functions #'wisi-prj-xref-backend 10))
-
-;;; Compiling SAL with standard .gpr, ada-mode 7.3.beta
-;;  (wisi-prj-select-cache "sal.prj" (create-ada-prj :name "sal stephe-2") "Makefile"))
-
-;;; Compiling SAL with Alire .gpr, eglot
 (wisi-prj-select-cache "sal.prj" (create-ada-prj :name "sal stephe-2") "Makefile")
 
-;; FIXME: turn off stuff that conflicts with eglot
+;; FIXME: use ada-eglot
+;; (setq-default
+;;  eglot-workspace-configuration
+;;  `((ada (projectFile . ,(expand-file-name "stephes_ada_library.gpr")))))
 
-(setq-default
- eglot-workspace-configuration
- `((ada (projectFile . ,(expand-file-name "stephes_ada_library.gpr")))))
-
-(eglot
- 'ada-lite-mode
- (wisi-prj-current-cached nil)
- 'eglot-lsp-server ;; class
- #'gnat-find-als ;; contact
- "Ada" ;; language-id
- )
+;; (eglot
+;;  'ada-lite-mode
+;;  (wisi-prj-current-cached nil)
+;;  'eglot-lsp-server ;; class
+;;  #'gnat-find-als ;; contact
+;;  "Ada" ;; language-id
+;;  )
 
 ;; end of file
