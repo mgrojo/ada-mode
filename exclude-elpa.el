f24700b7d677dd930f5f5f6221eaa212a2f4c5bc
Stephen Leake <stephen_leake@stephe-leake.org>
2022-09-17 01:34:15 -0700

    * exclude-elpa.el: Allow mmm-mode

--- exclude-elpa.el
+++ exclude-elpa.el
@@ -1,4 +1,6 @@
-;; Disable installed ELPA packages
-(defvar package-load-list nil)
+;; Disable installed ELPA packages except mmm-mode
+(defvar package-load-list '(all))
+(push '(ada-mode . nil) package-load-list)
+(push '(wisi . nil) package-load-list)
 
 ;; end of file
