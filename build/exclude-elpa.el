e43d134af25cff0378f602cc770a85381a821daa
Stephen Leake <stephen_leake@stephe-leake.org>
2022-11-03 14:51:48 -0700

    Resolve conflicts, finish propagate

--- build/exclude-elpa.el
+++ build/exclude-elpa.el
@@ -3,6 +3,9 @@
 (push '(ada-mode . nil) package-load-list)
 (push '(wisi . nil) package-load-list)
 
+;; FIXME: testing devel eglot
+(push '(eglot . nil) package-load-list)
+
 ;; also disable native compilation; it just slows things down
 (setq native-comp-deferred-compilation nil)
 
--- build/run-indent-test-eglot.el
+++ build/run-indent-test-eglot.el
@@ -4,7 +4,7 @@
 
 (setq ada-xref-backend 'eglot)
 (setq ada-indent-backend 'none) ;; als 22 doesn't support range indent
-(setq ada-face-backend 'none) ;; als 22 doesn't support semantic_tokens
+(setq ada-face-backend 'eglot) ;; als 22 doesn't support semantic_tokens; FIXME: testing devel version
 
 (setq skip-reindent-test (eq ada-indent-backend 'none))
 (setq skip-recase-test   (eq ada-face-backend 'none))
