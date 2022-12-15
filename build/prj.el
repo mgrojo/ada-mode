a0c4855cd214d0ed79f4a56d032ebb40ce32681f
Stephen Leake <stephen_leake@stephe-leake.org>
2022-12-15 03:15:16 -0800

    * build/prj.el ("gpr_mode_wisi_parse.prj"): Add GNAT_COMPILER

--- build/prj.el
+++ build/prj.el
@@ -6,6 +6,7 @@
   :name "gpr_mode_wisi_parse main"
   :compile-env
   (list
+   (concat "GNAT_COMPILER=" (expand-file-name "../../elpa/packages/gnat-compiler"))
    (concat "SAL=" (expand-file-name "../../org.stephe_leake.sal"))
    (concat "WISITOKEN=" (expand-file-name "../../org.wisitoken"))
    (concat "WISI=" (expand-file-name "../../org.emacs.wisi"))
