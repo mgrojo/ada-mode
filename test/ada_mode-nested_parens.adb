734cabd6e33ccd76036503919591c6598d203a3f
Stephen Leake <stephen_leake@stephe-leake.org>
2023-06-18 14:28:49 -0700

    Eliminate a conflict that was causing problems for nested aggregates
    * .gdbinit: Improve.
    
    * ada_annex_p.wy (%conflicts): Match changes.
    (%conflicts): Eliminate 'REDUCE subtype_indication | REDUCE primary on
    token BAR'.
    (subtype_indication): Exclude simple 'name'; that was causing the
    conflict.
    (subtype_declaration, object_declaration, derived_type_definition)
    (discrete_subtype_definition, component_definition, discrete_range)
    (discrete_choice, access_to_object_definition,
    loop_parameter_subtype_indication)
    (return_subtype_indication, private_extension_declaration): Match change
    to subtype_indication.
    
    * build/Makefile (all): Improve.
    (one): Improve declaration of TEST_FILE.
    (two-lr1): Improve declaration of VERBOSITY.
    (test-ada-lalr-partial-process-gpr_query.stamp): Improve.
    (test-ada-lalr-incremental-process-gpr_query.stamp):
    
    * test/ada_mode-nested_parens.adb: Add worse nested aggregate; now parses
    without error.

--- test/ada_mode-nested_parens.adb
+++ test/ada_mode-nested_parens.adb
@@ -1,4 +1,4 @@
--- From Eurocontrol; gets "too many parallel parsers required" with
+-- Example statements that get "too many parallel parsers required" with
 -- default 20 limit
 --
 -- FIXME: the number of parsers required grows geometrically with the
@@ -6,13 +6,24 @@
 -- increasing wisi-parse-max-parallel is not a long-term solution; we
 -- need to optimize the grammar in this area. ada-mode 7.1.6 did not
 -- have this problem.
---EMACS_SKIP_UNLESS: nil
 procedure Ada_Mode.Nested_Parens
 is
+   -- From Eurocontrol
    Headers : aliased constant Headers_T
      := ((new String'("ARCID"), ((Field_T'Pos (Aircraft),      Text_Kind), others => None_Column), True),
          (new String'("Aty"),   ((Field_T'Pos (Aircraft_Type), Text_Kind), others => None_Column), True),
          (new String'("Aty"),   ((Field_T'Pos (Aircraft_Type), Text_Kind), others => None_Column), True));
 begin
-   null;
+   --  From WisiToken generated subprograms actions.
+   Indent_Action_0
+     (Parse_Data, Tree, Nonterm,
+      (T1 => (False, (Simple, (Label => None))),
+       T2 => (False, (Simple, (Label => None))),
+       T3 => (False, (Simple, (Label => None))),
+       T4 => (False, (Simple, (Int, Subp_Indent))),
+       T5 => (False, (Simple, (Label => None))),
+       T6 => (False, (Simple, (Int, Subp_Indent))),
+       T7 => (False, (Simple, (Label => None))),
+       T8 => (False, (Simple, (Label => None))),
+       T9 => (False, (Simple, (Label => None)))));
 end Ada_Mode.Nested_Parens;
