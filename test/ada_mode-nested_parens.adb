389998e42e803831c350b4bcc0cb087999c45478
Stephen Leake <stephen_leake@stephe-leake.org>
2023-10-06 01:36:12 -0700

    * test/ada_mode-nested_parens.adb (): Delete FIXME: now fixed

--- test/ada_mode-nested_parens.adb
+++ test/ada_mode-nested_parens.adb
@@ -1,11 +1,5 @@
--- Example statements that get "too many parallel parsers required" with
--- default 20 limit
---
--- FIXME: the number of parsers required grows geometrically with the
--- nesting level, not the number of elements in the aggregate. So
--- increasing wisi-parse-max-parallel is not a long-term solution; we
--- need to optimize the grammar in this area. ada-mode 7.1.6 did not
--- have this problem.
+-- Example statements that used to get "too many parallel parsers
+-- required".
 procedure Ada_Mode.Nested_Parens
 is
    -- From Eurocontrol
